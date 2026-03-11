b3bb3ff5ee546fde9d58a8909f4d958fd867ce1c
Divam2398 <117377570+Divam2398@users.noreply.github.com>
2026-03-11 10:36:07 +0530

    Update salary_prediction_deployment.py

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -2,49 +2,31 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-# Load model and encoders
-model = joblib.load("salary_prediction_rfr_model.pkl")
-encoder = joblib.load("label_encoder_salary.pkl")
+model = joblib.load('salary_prediction_rfr_model.pkl')
+encoder = joblib.load('label_encoder_Salary.pkl')
 
-st.title("Salary Prediction Model")
+st.title('Salary Prediction model')
 
-# Inputs
-age = st.number_input("Enter Age", min_value=18, max_value=65)
+age = st.number_input("Enter your age", 18, 65)
+gender = st.selectbox("Select your gender", encoder["Gender"].classes_)
+education = st.selectbox("Select your education", encoder["Education Level"].classes_)
+job_title = st.selectbox("Select your job title", encoder["Job Title"].classes_)
+experience = st.number_input("Enter your experiene(in years)", 0, 50)
 
-gender = st.selectbox(
-    "Select Gender",
-    encoder["Gender"].classes_
-)
+# IMPORTANT: column names must match training dataset
+df = pd.DataFrame({
+    "Age":[age],
+    "Gender":[gender],
+    "Education Level":[education],
+    "Job Title":[job_title],
+    "Years of Experience":[experience]
+})
 
-education = st.selectbox(
-    "Select Education Level",
-    encoder["Education Level"].classes_
-)
+if st.button("predict salary"):
 
-job = st.selectbox(
-    "Select Job Title",
-    encoder["Job Title"].classes_
-)
+    for col in ["Gender","Education Level","Job Title"]:
+        df[col] = encoder[col].transform(df[col])
 
-# Encode values
-gender_encoded = encoder["Gender"].transform([gender])[0]
-education_encoded = encoder["Education Level"].transform([education])[0]
-job_encoded = encoder["Job Title"].transform([job])[0]
+    prediction = model.predict(df)
 
-if st.button("Predict Salary"):
-
-    # Create dataframe
-    input_data = pd.DataFrame({
-        "Age": [age],
-        "Gender": [gender_encoded],
-        "Education Level": [education_encoded],
-        "Job Title": [job_encoded]
-    })
-
-    # Ensure column order matches training
-    input_data = input_data[model.feature_names_in_]
-
-    # Prediction
-    prediction = model.predict(input_data)
-
-    st.success(f"Predicted Salary: ₹{prediction[0]:,.2f}")
+    st.success(f"predicted salary: {prediction[0]}")
